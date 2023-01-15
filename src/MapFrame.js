68b7b988c779c046e49ed41aa33c564968cc72fe
Triendster <tom-wanning@gmx.net>
2023-01-15 16:55:57 +0100

    modified:   src/MapFrame.js

--- src/MapFrame.js
+++ src/MapFrame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './MapFrame.css'
 
 
-const address = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.1400694057907!2d11.60606861574715!3d50.88411787953752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6aec549107877%3A0xe0ef55ea198e991e!2sAsia-Wok-Bistro!5e0!3m2!1sde!2sde!4v1663935723518!5m2!1sde!2sde"
+// const address = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2517.1400694057907!2d11.60606861574715!3d50.88411787953752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a6aec549107877%3A0xe0ef55ea198e991e!2sAsia-Wok-Bistro!5e0!3m2!1sde!2sde!4v1663935723518!5m2!1sde!2sde"
 
 function MapFrame () {
   return(
@@ -12,7 +12,7 @@ function MapFrame () {
           <h1>Besuchen Sie uns vor Ort</h1>
         </div>
         <div className='mapframe-content-map'>
-          <iframe title='Asia-Wok-Bistro Karte' src={address} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>
+          {/* <iframe title='Asia-Wok-Bistro Karte' src={address} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/> */}
         </div>
         <div className='mapframe-content-contacts'>
           <div className='mapframe-contacs-content'>
