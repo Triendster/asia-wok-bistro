f052de9baad82671e6043b7c2a80a36a833a5d86
Triendster <tom-wanning@gmx.net>
2023-01-15 18:19:55 +0100

    modified:   src/MapFrame.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './Navbar'
+import Title from './Title'
+import News from './News';
+import Gallery from './Gallery'
+import MapFrame from './MapFrame';
+import Footer from './Footer';
+import SideNav from './SideNav';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Asia wok bistro</h1>
+    <SideNav />
+    <Navbar />
+    <Title />
+    <News />
+    <Gallery />
+    <MapFrame />
+    <Footer />
   </React.StrictMode>
 );
 
